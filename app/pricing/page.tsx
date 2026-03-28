2f5a0951c2de92f2b3465eb7adf348f7e0e408f3
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-03-28 15:01:41 +0100

    Create page.tsx

---
+++ app/pricing/page.tsx
@@ -0,0 +1,19 @@
+export default function Pricing() {
+  return (
+    <div>
+      <h1>Pricing</h1>
+
+      <a href="https://radhiayt.gumroad.com/l/bcnma">
+        <button>$99 Starter</button>
+      </a>
+
+      <a href="https://radhiayt.gumroad.com/l/gmoyee">
+        <button>$299 Growth</button>
+      </a>
+
+      <a href="https://radhiayt.gumroad.com/l/dfhqqg">
+        <button>$799 Scale</button>
+      </a>
+    </div>
+  )
+}
