f7542dc8d11b66b7726e4523b3535791b008df26
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-03-28 15:36:45 +0100

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,19 +1,28 @@
 export default function Pricing() {
   return (
-    <div>
-      <h1>Pricing</h1>
+    <div className="flex gap-6">
 
       <a href="https://radhiayt.gumroad.com/l/bcnma">
-        <button>$99 Starter</button>
+        <div className="bg-gray-900 p-6 rounded-xl">
+          <h2>Starter</h2>
+          <p>$99</p>
+        </div>
       </a>
 
       <a href="https://radhiayt.gumroad.com/l/gmoyee">
-        <button>$299 Growth</button>
+        <div className="bg-gray-900 p-6 rounded-xl">
+          <h2>Growth</h2>
+          <p>$299</p>
+        </div>
       </a>
 
       <a href="https://radhiayt.gumroad.com/l/dfhqqg">
-        <button>$799 Scale</button>
+        <div className="bg-gray-900 p-6 rounded-xl">
+          <h2>Scale</h2>
+          <p>$799</p>
+        </div>
       </a>
+
     </div>
   )
 }
